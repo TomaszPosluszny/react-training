57b4a1938a7589e9c9c01863dca8b8b5d4511a81
TomaszPosluszny <115580686+TomaszPosluszny@users.noreply.github.com>
2023-04-23 10:21:38 +0200

    JS in components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default class HelloWorldComponent extends React.Component {
   render() {
-    return ( <h1>Hello World !!!</h1>
+    return ( <h1>Hello World !!!{2+2} ale super</h1>
       )
     }
   }
