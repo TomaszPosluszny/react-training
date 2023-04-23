4449cb8e82f3e67177953aca9447ac2dca4cc701
TomaszPosluszny <115580686+TomaszPosluszny@users.noreply.github.com>
2023-04-23 10:14:01 +0200

    Add  three new components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React from "react";
 
-export default class Tomek extends React.Component {
+export default class HelloWorldComponent extends React.Component {
   render() {
-    return (<button>Kliknij mnie</button>
+    return ( <h1>Hello World !!!</h1>
       )
     }
   }
+
+  export default class MyTextComponent extends React.Component {
+    render() {
+      return ( <p>Start editing to see some magic happen :)</p>
+        )
+      }
+    }
+    export default class GreetingsComponent extends React.Component {
+      render() {
+        return (
+        <>
+        <h2>I love React!</h2>
+        <h2>I great technology!</h2>
+        </>
+          )
+        }
+      }
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World!!!</h1>
-        <h1>Hej!!!</h1>
+        <HelloWorldComponent/>
+        <HelloWorldComponent/>
+       
 
         <hr />
-<Tomek/>
-        <h2>I love React!</h2>
-        <h2>I great technology!</h2>
 
+<GreetingsComponent/>
         <hr />
 
         <h3>Hello Wiktor!</h3>
@@ -27,7 +43,7 @@ export default class App extends React.Component {
 
         <hr />
 
-        <p>Start editing to see some magic happen :)</p>
+        <MyTextComponent/>
       </div>
     );
   }
